fc8fb7df4aedf5ee51ae104ebf07fad7727889c4
DivyanshMDev <22052813@kiit.ac.in>
2024-08-25 00:13:09 +0530

    clerk auth completed and initialized Sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import {IBM_Plex_Sans  } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider,SignedIn,SignedOut,UserButton,SignInButton } from "@clerk/nextjs";
 
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
@@ -14,15 +14,35 @@ export const metadata: Metadata = {
   description: "AI powered image generator",
 };
 
+
+function Header() {
+  return (
+    <header style={{ display: 'flex', justifyContent: 'space-between', padding: 20 }}>
+      <h1>My App</h1>
+      <SignedIn>
+        {/* Mount the UserButton component */}
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        {/* Signed out users get sign in button */}
+        <SignInButton />
+      </SignedOut>
+    </header>
+  )
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-      <ClerkProvider>
+      <ClerkProvider appearance={{
+        variables:{colorPrimary: '#624cf5'}
+      }}>
     <html lang="en">
       <body className={cn("font-IBMPlex antialiased",IBMPlex.variable)}>
+      <Header />
       {children}
       </body>
     </html>

ee085c3b433592af8a4ee61191f0253f90a3559e
DivyanshMDev <22052813@kiit.ac.in>
2024-10-04 17:13:28 +0530

    started mobile nav
    completed side bar part and started mobilenav

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -2,10 +2,12 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { SignedIn } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { navLinks } from '@/constants'
 import { group } from 'console'
 import { usePathname } from 'next/navigation'
+import { Button } from '../ui/button'
+
 const Sidebar = () => {
   const pathname=usePathname();
   return (
@@ -20,20 +22,58 @@ const Sidebar = () => {
         <nav className='sidebar-nav'>
         <SignedIn>
           <ul className='sidebar-nav_elements'>
-          {navLinks.map((link)=>
+          {navLinks.slice(0,6).map((link)=>
           {
-            const isActive=link.route ===pathname
+            const isActive=link.route ===pathname;
 
-
+            //instead of group={isActive} ${isActive} has been used taki background 
             return (
-              <li key={link.route} className={`sidebar-nav_element group={isActive?
-                'bg-purple-gradient text-white': 'text-gray-700'}`}>
-                  {link.label}
+              <li key={link.route} className={`sidebar-nav_element ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
+                
+                   <Link className="sidebar-link" href={link.route} >
+                   <Image src={link.icon} 
+                   alt="logo"
+                   width={24}
+                   height={24}
+                   className={`${isActive && 'brightness-200'}`} />
+                  {link.label}</Link>
                   </li>
             )
           })}
           </ul>
-        </SignedIn></nav>
+
+
+
+          <ul className='sidebar-nav_elements'>
+          {navLinks.slice(6).map((link)=>
+          {
+            const isActive=link.route ===pathname;
+            //done so that profile and buy credits shift downwards along woth logo from 6 index onwards
+            return (
+              <li key={link.route} className={`sidebar-nav_element ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
+                
+                   <Link className="sidebar-link" href={link.route} >
+                   <Image src={link.icon} 
+                   alt="logo"
+                   width={24}
+                   height={24}
+                   className={`${isActive && 'brightness-200'}`} />
+                  {link.label}</Link>
+                  </li>
+            )
+          })}
+          <li className="flex-center cursor-pointer gap-2 p-4">
+          <UserButton showName/>
+          </li>
+          </ul>
+        </SignedIn>
+
+        <SignedOut>
+          <Button asChild className="button bg-purple-gradient bg-cover">
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </SignedOut>
+        </nav>
       </div>
     </aside>
   )

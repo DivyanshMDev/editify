d7bb35a5be1ad6017d24342dd300ecc06b45f0f8
DivyanshMDev <22052813@kiit.ac.in>
2024-10-05 02:34:03 +0530

    updated mobile nav

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,15 +1,18 @@
+"use client"
 import React from 'react'
 import {
     Sheet,
     SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
   import Link from 'next/link'
   import Image from 'next/image'
+  import { navLinks } from '@/constants'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { usePathname } from 'next/navigation'
+import { Button } from '../ui/button'
 const MobileNav = () => {
+    const pathname=usePathname();
   return (
     
     <header className="header">
@@ -21,7 +24,55 @@ const MobileNav = () => {
     height={28}/>
         </Link>
 
-        
+    <nav className='flex gap-2'>
+        <SignedIn>
+            <UserButton></UserButton>
+            <Sheet>
+  <SheetTrigger>
+    <Image src="/assets/icons/menu.svg"
+    alt="menu"
+    width={32}
+    height={32}
+    className='cursor-pointer'></Image>
+  </SheetTrigger>
+  <SheetContent className='sheet-content sm:w-64'>
+    <>
+    <Image
+    src="/assets/images/logo-text.svg"
+    alt="logo"
+    width={152}
+    height={23}/>
+    <ul className='header-nav_elements'>
+          {navLinks.map((link)=>
+          {
+            const isActive=link.route ===pathname;
+
+            //instead of group={isActive} ${isActive} has been used taki background 
+            return (
+              <li key={link.route} className={`${isActive && 'gradient-text'} p-18 flex whitespace-nowrap text-dark-700`}>
+                
+                   <Link className="sidebar-link" href={link.route} >
+                   <Image src={link.icon} 
+                   alt="logo"
+                   width={24}
+                   height={24}
+                   className={`${isActive && 'brightness-200'}`} />
+                  {link.label}</Link>
+                  </li>
+            )
+          })}
+    </ul>
+    </>
+  </SheetContent>
+</Sheet>
+
+        </SignedIn>
+        <SignedOut>
+          <Button asChild className="button bg-purple-gradient bg-cover">
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </SignedOut>
+        </nav>    
     </header>
   )
 }
